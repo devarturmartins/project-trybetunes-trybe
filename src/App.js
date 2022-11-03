3491a7414b9e9457000c55a85c22aec3f968bce0
devarturmartins <arturvl1@hotmail.com>
2022-11-03 14:56:54 -0300

    r. 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes </p>);
+    return (
+      <div>
+        <header>
+          TrybeTunes
+        </header>
+        <Login />
+        <Search />
+        <Album />
+        <Favorites />
+        <Profile />
+        <ProfileEdit />
+        <NotFound />
+      </div>
+    );
   }
 }
 
