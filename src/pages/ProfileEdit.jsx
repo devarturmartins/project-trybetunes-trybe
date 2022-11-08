5dc918e2c135a101c47e274929f6b0f73fd8a12e
devarturmartins <arturvl1@hotmail.com>
2022-11-08 12:47:22 -0300

    requisitos nao terminados

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import Header from '../Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class ProfileEdit extends Component {
+  state = {
+    user: {},
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.editProfile();
+  }
+
+  editProfile = async () => {
+    const user = await getUser();
+    this.setState({ user });
+  };
+
   render() {
+    const { loading, user } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        {
+          loading ? <Loading />
+            : (
+              <form>
+                <input data-testid="edit-input-name" />
+                <input data-testid="edit-input-email" />
+                <input data-testid="edit-input-description" />
+                <input data-testid="edit-input-image" />
+                <button type="button" data-testid="edit-button-save">Salvar</button>
+              </form>
+            )
+        }
       </div>
     );
   }
